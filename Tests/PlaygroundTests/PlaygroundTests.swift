812c51d58f9958eb4d8451904e502c976da38751
Dominic Frei <dominic.frei@mongodb.com>
2021-01-06 17:03:36 +0000

    Example for realm/realm-cocoa/issues/7028.

--- Tests/PlaygroundTests/PlaygroundTests.swift
+++ Tests/PlaygroundTests/PlaygroundTests.swift
@@ -11,13 +11,25 @@ final class PlaygroundTests: XCTestCase {
         _ = try! Realm.deleteFiles(for: Realm.Configuration.defaultConfiguration)
     }
     
-    override func tearDown() {
-        _ = try! Realm.deleteFiles(for: Realm.Configuration.defaultConfiguration)
-    }
-    
     func testExample() {
         let realm = try! Realm()
-        
+        let object = TestObject()
+        try! realm.write {
+            realm.add(object)
+        }
+        let list = List<TestObject>()
+        list.append(object)
+        let expectation = XCTestExpectation()
+        DispatchQueue.init(label: "queue1").async {
+            let realm = try! Realm()
+            try! realm.write {
+                for object in list {
+                    realm.delete(object)
+                }
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 3.0)
     }
     
 }
