f9918f9636e1a3020660367a81979e47146c5e03
Dominic Frei <dominic.frei@mongodb.com>
2021-01-13 17:53:30 +0000

    Tutorial implementation.

--- Tests/PlaygroundTests/PlaygroundTests.swift
+++ Tests/PlaygroundTests/PlaygroundTests.swift
@@ -1,23 +1,47 @@
 import XCTest
-import RealmSwift
-
-class TestObject: Object {
-    @objc dynamic var foo = 1
-}
+import class Foundation.Bundle
 
 final class PlaygroundTests: XCTestCase {
-    
-    override func setUp() {
-        _ = try! Realm.deleteFiles(for: Realm.Configuration.defaultConfiguration)
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+
+        // Some of the APIs that we use below are available in macOS 10.13 and above.
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("Playground")
+
+        let process = Process()
+        process.executableURL = fooBinary
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)
+
+        XCTAssertEqual(output, "Hello, world!\n")
     }
-    
-    override func tearDown() {
-        _ = try! Realm.deleteFiles(for: Realm.Configuration.defaultConfiguration)
+
+    /// Returns path to the built products directory.
+    var productsDirectory: URL {
+      #if os(macOS)
+        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
+            return bundle.bundleURL.deletingLastPathComponent()
+        }
+        fatalError("couldn't find the products directory")
+      #else
+        return Bundle.main.bundleURL
+      #endif
     }
-    
-    func testExample() {
-        let realm = try! Realm()
-        
-    }
-    
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
 }
