ad031f4001a0282832488e1553bb57a2acef560f
Dominic Frei <dominic.frei@mongodb.com>
2021-01-21 12:19:29 +0000

    Create new Xcode 11.1 project: macOS -> App -> default settings.

---
+++ PlaygroundApp/PlaygroundApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PlaygroundApp
+//
+//  Created by Dominic Frei on 21/01/2021.
+//  Copyright © 2021 Dominic Frei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
