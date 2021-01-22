cf003b77703cad74c5fbc12aa0b08d6f568ba2cf
Dominic Frei <dominic.frei@mongodb.com>
2021-01-22 10:39:45 +0000

    Empty SwiftUI playground.

---
+++ PlaygroundApp/PlaygroundApp/PlaygroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundAppApp.swift
+//  PlaygroundApp
+//
+//  Created by Dominic Frei on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
