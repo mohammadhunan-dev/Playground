80cef4a29c58f6b687a0b89df87f8a39fe7f1859
Dominic Frei <dominic.frei@mongodb.com>
2021-02-19 15:12:29 +0000

    Add multiplatform app template project (settings see screenshot).

---
+++ PlaygroundApp/Shared/PlaygroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundAppApp.swift
+//  Shared
+//
+//  Created by Dominic Frei on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
