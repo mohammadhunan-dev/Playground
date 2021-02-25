3b9e1dce92ff05a112a42a59b58fb136faa08c2f
Dominic Frei <dominic.frei@mongodb.com>
2021-02-25 18:49:19 +0000

    Add sample code to reproduce the issue.

--- Playground/Playground/ContentView.swift
+++ Playground/Playground/ContentView.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import RealmSwift
+
+class TestObject: Object {
+    @objc dynamic var foo = 42
+}
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
+    
+    init() {
+        Realm.Configuration.defaultConfiguration = Realm.Configuration(encryptionKey: Data(count: 64))
+        let realm = try! Realm()
+        for _ in 0..<30000 {
+            try! realm.write {
+                realm.add(TestObject())
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
