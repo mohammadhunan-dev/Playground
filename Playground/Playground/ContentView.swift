1afbf412409a6e4873ad89535945a82c85ed3842
Dominic Frei <dominic.frei@mongodb.com>
2021-02-25 18:45:01 +0000

    Add iOS App project from template (untouched), including the project setup config.

---
+++ Playground/Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Playground
+//
+//  Created by Dominic Frei on 25/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Playground/Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Dominic Frei on 25/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
